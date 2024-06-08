fb27c028744fc5d9f91b01ca1c2b49c9293ee11c
Galuh1305 <galuh_abada@teknokrat.ac.id>
2024-06-09 02:34:06 +0700

    master: menghubungkan dari login ke menu,menggunakan uname dan pass

--- program/app/src/main/java/com/uti/pm_panduan_cabai/MainActivity.kt
+++ program/app/src/main/java/com/uti/pm_panduan_cabai/MainActivity.kt
@@ -10,6 +10,7 @@ import android.widget.Toast;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.content.Intent;
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,6 +19,34 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        // EditText untuk username dan password
+        val usernameEditText = findViewById<EditText>(R.id.eduser)
+        val passwordEditText = findViewById<EditText>(R.id.edpass)
+
+        // Set nilai default untuk username dan password
+        usernameEditText.setText("")
+        passwordEditText.setText("")
+
+        // Tombol login
+        val loginButton = findViewById<Button>(R.id.login)
+
+        // Tambahkan listener klik pada tombol login
+        loginButton.setOnClickListener {
+            // Dapatkan nilai username dan password dari EditText
+            val username = usernameEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+            // Lakukan validasi pada username dan password
+            if (username == "admin" && password == "admin") {
+                // Jika username dan password benar, lanjutkan ke aktivitas utama
+                val intent = Intent(this, MenuActivity::class.java)
+                startActivity(intent)
+            } else {
+                // Jika username dan password salah, tampilkan pesan kesalahan
+                Toast.makeText(this, "Username atau password salah", Toast.LENGTH_SHORT).show()
+            }
+        }
 
 
     }
---
+++ program/app/src/main/java/com/uti/pm_panduan_cabai/MenuActivity.kt
@@ -0,0 +1,15 @@
+package com.uti.pm_panduan_cabai
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_menu)
+
+        // Tambahkan kode untuk membuat tombol dan listener klik di sini
+    }
+}
