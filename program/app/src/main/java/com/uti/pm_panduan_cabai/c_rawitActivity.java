517a2cea1d376a46c9b415e9e881e716567e439e
Galuh1305 <galuh_abada@teknokrat.ac.id>
2024-06-13 19:26:33 +0700

    master: menambahkan button back/kembali di rawitactivity

--- program/app/src/main/java/com/uti/pm_panduan_cabai/c_rawitActivity.java
+++ program/app/src/main/java/com/uti/pm_panduan_cabai/c_rawitActivity.java
@@ -2,11 +2,32 @@ package com.uti.pm_panduan_cabai;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class c_rawitActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_c_rawit);
+
+        // Inisialisasi Button
+        Button buttonBack = findViewById(R.id.back);
+
+        // Set onClickListener untuk Button
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Kembali ke Activity sebelumnya
+                onBackPressed();
+            }
+        });
+    }
+
+    // Mengoverride fungsi onBackPressed jika Anda ingin menambahkan logika khusus saat tombol "Back" ditekan
+    @Override
+    public void onBackPressed() {
+        // Tambahkan logika khusus di sini jika diperlukan sebelum kembali
+        super.onBackPressed();  // Ini akan memanggil aksi default, yaitu kembali ke Activity sebelumnya
     }
 }
