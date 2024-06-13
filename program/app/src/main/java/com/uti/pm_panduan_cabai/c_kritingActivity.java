c37613e6eaf884c3d9aab454e4257901bd7ef011
Galuh1305 <galuh_abada@teknokrat.ac.id>
2024-06-13 23:56:40 +0700

    master: Membuat activity c_keriting

---
+++ program/app/src/main/java/com/uti/pm_panduan_cabai/c_kritingActivity.java
@@ -0,0 +1,12 @@
+package com.uti.pm_panduan_cabai;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class c_kritingActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_c_kriting);
+    }
+}
