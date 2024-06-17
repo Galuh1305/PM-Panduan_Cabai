f290dc5ef20f8462f28c08060b779b2394839359
Galuh1305 <galuh_abada@teknokrat.ac.id>
2024-06-17 17:51:03 +0700

    master: menghubungkan keluar agar bisa diclick

--- program/app/build.gradle.kts
+++ program/app/build.gradle.kts
@@ -38,7 +38,7 @@ android {
 dependencies {
 
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.appcompat) // Pastikan ini sesuai dengan yang Anda inginkan atau bisa dihapus jika redundant
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
